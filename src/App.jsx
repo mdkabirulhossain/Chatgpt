629237a009cbc5f0947d69d3164aeb1e2cd173f8
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-06-29 01:18:47 +0600

    chatgpt clone successfully

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import './App.css'
 import logo from './assets/chatgpt.svg'
 import addimg from './assets/add-30.png'
@@ -8,15 +8,44 @@ import saved from './assets/bookmark.svg'
 import upgrade from './assets/rocket.svg'
 import gptlogo from './assets/chatgptLogo.svg'
 import send from './assets/send.svg'
+import userlogo from './assets/user-icon.png'
 import { sendMsgToOpenAI } from './openAI'
 
 function App() {
 
+  const msgEnd = useRef(null)
   const [input, setInput] = useState("");
+  const [message, setMessage] = useState([
+    {
+      text: "Hi, I am chatGpt! How can I assist you?",
+      isBot: true,
+    }
+  ])
 
-  const handleSend = async() =>{
-    const res = await sendMsgToOpenAI(input);
+  useEffect(()=>{
+    msgEnd.current.scrollIntoView();
+  }, [message])
+
+  const handleSend = async () => {
+    const text = input;
+    setInput('');
+     setMessage(prev => [...prev, { text, isBot: false }]);
+    const res = await sendMsgToOpenAI(text);
     console.log(res);
+    setMessage(prev => [...prev, { text: res, isBot: true }]);
+  }
+
+  const handleEnter = async(e) =>{
+    if(e.key === 'Enter') await handleSend();
+  }
+
+  const handleQuery = async(e) =>{
+    const text = e.target.value;
+    setInput('');
+     setMessage(prev => [...prev, { text, isBot: false }]);
+    const res = await sendMsgToOpenAI(text);
+    console.log(res);
+    setMessage(prev => [...prev, { text: res, isBot: true }]);
   }
 
   return (
@@ -27,10 +56,10 @@ function App() {
             <img src={logo} alt="" />
             <span className='text-3xl'>ChatGPT</span>
           </div>
-          <button className='flex gap-1 p-3 items-center justify-center w-full mx-auto rounded-md bg-[#5A4BFF] mb-12'><img src={addimg} className='w-7 h-7' alt="" /><span className='text-xl'>New Chat</span></button>
+          <button onClick={()=>{window.location.reload()}} className='flex gap-1 p-3 cursor-pointer items-center justify-center w-full mx-auto rounded-md bg-[#5A4BFF] mb-12'><img src={addimg} className='w-7 h-7' alt="" /><span className='text-xl'>New Chat</span></button>
           <div className='flex flex-col gap-2'>
-            <button className='flex gap-4 items-center p-6 rounded-md border border-[rgba(98,98,98,1)]'><img src={msgicon} alt="" /> What is Programming ?</button>
-            <button className='flex gap-4 items-center p-6 rounded-md border border-[rgba(98,98,98,1)]'><img src={msgicon} alt="" /> How to use an API ?</button>
+            <button className='flex gap-4 items-center cursor-pointer p-6 rounded-md border border-[rgba(98,98,98,1)]' value={"What is Programming ?"} onClick={handleQuery}><img src={msgicon} alt="" /> What is Programming ?</button>
+            <button className='flex gap-4 items-center cursor-pointer p-6 rounded-md border border-[rgba(98,98,98,1)]' value={"How to use an API ?"} onClick={handleQuery}><img src={msgicon} alt="" /> How to use an API ?</button>
           </div>
         </div>
         <div className="lowerSide p-10 border-t border-t-red-50">
@@ -40,21 +69,33 @@ function App() {
         </div>
       </div>
       <div className="main flex-1 flex flex-col mx-20 my-10">
-        <div className='chats overflow-hidden overflow-y-scroll scroll-smooth w-full h-[calc(100vh_-_17rem)]'>
-          <div className="flex ">
-            {/* <img src={logo} alt="" /> */}
-            <h3 className='mx-auto text-2xl'>Where should we begin?</h3>
+         <h3 className='mx-auto text-2xl'>Where should we begin?</h3>
+        <div className='chats overflow-y-auto scroll-smooth w-full h-[calc(100vh_-_12rem)]'>
+          
+          {/* <div className="chat flex items-start gap-2 mt-6 p-3 rounded-md ">
+            <img src={userlogo} className='w-6 h-6' alt="" />
+            <p className='text-justify'>Lorem ipsum dolor sit </p>
           </div>
+
           <div className="chat flex items-start gap-2 mt-6 p-3 rounded-md bg-[#1c1e3a]">
             <img src={gptlogo} className='w-6 h-6' alt="" />
             <p className='text-justify'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis fugiat ipsam eveniet suscipit incidunt non sit atque ipsa ratione, illum nam veniam? In architecto est repellendus nemo eaque voluptatem quas modi quisquam quod veritatis itaque, accusamus tempora facilis dolorem rem commodi culpa? Molestiae quis quibusdam repellat harum earum porro adipisci!</p>
-          </div>
+          </div> */}
+          {
+            message.map((msg, index) => (
+              <div key={index} className={msg.isBot ? "chat flex items-start gap-2 mt-6 p-3 rounded-md bg-[#1c1e3a]": "chat flex items-start gap-2 mt-6 p-3 rounded-md "}>
+                <img src={ msg.isBot? gptlogo : userlogo} className='w-6 h-6' alt="" />
+                <p className='text-justify'>{msg.text}</p>
+              </div>
+            ))
+          }
+          <div ref={msgEnd} />
 
         </div>
         <div className='chatFooter mt-auto w-full flex items-center justify-center p-4 rounded-md bg-[#1c1e3a]'>
           <input className='w-full outline-none ' type="text" placeholder='Send a message'
-          value={input} onChange={(e) => setInput(e.target.value)}
-
+            value={input} onChange={(e) => setInput(e.target.value)}
+            onKeyDown={handleEnter}
           />
           <button className='cursor-pointer' onClick={handleSend}><img src={send} alt="" /></button>
         </div>
