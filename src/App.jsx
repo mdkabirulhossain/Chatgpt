21dd745361ece909cc04f8f032a870230ce22561
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-06-28 16:18:23 +0600

    intall OpenAI

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import msgicon from './assets/message.svg'
 import home from './assets/home.svg'
 import saved from './assets/bookmark.svg'
 import upgrade from './assets/rocket.svg'
+import gptlogo from './assets/chatgptLogo.svg'
+import send from './assets/send.svg'
 
 function App() {
 
@@ -24,13 +26,27 @@ function App() {
           </div>
         </div>
         <div className="lowerSide p-10 border-t border-t-red-50">
-            <div className='flex gap-4 items-center m-3 py-1'><img src={home} className='min-w-5' alt="" />Home</div>
-            <div className='flex gap-4 items-center m-3 py-1'><img src={saved} className='min-w-5' alt="" />Saved</div>
-            <div className='flex gap-4 items-center m-3 py-1'><img src={upgrade} className='min-w-5' alt="" />Upgrade to Pro</div>
+          <div className='flex gap-4 items-center m-3 py-1'><img src={home} className='min-w-5' alt="" />Home</div>
+          <div className='flex gap-4 items-center m-3 py-1'><img src={saved} className='min-w-5' alt="" />Saved</div>
+          <div className='flex gap-4 items-center m-3 py-1'><img src={upgrade} className='min-w-5' alt="" />Upgrade to Pro</div>
         </div>
       </div>
-      <div className="main">
-               <h1>hi</h1>
+      <div className="main flex-1 flex flex-col mx-20 my-10">
+        <div className='chats overflow-hidden overflow-y-scroll scroll-smooth w-full h-[calc(100vh_-_17rem)]'>
+          <div className="flex ">
+            {/* <img src={logo} alt="" /> */}
+            <h3 className='mx-auto text-2xl'>Where should we begin?</h3>
+          </div>
+          <div className="chat flex items-start gap-2 mt-6 p-3 rounded-md bg-[#1c1e3a]">
+            <img src={gptlogo} className='w-6 h-6' alt="" />
+            <p className='text-justify'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis fugiat ipsam eveniet suscipit incidunt non sit atque ipsa ratione, illum nam veniam? In architecto est repellendus nemo eaque voluptatem quas modi quisquam quod veritatis itaque, accusamus tempora facilis dolorem rem commodi culpa? Molestiae quis quibusdam repellat harum earum porro adipisci!</p>
+          </div>
+
+        </div>
+        <div className='chatFooter mt-auto w-full flex items-center justify-center p-4 rounded-md bg-[#1c1e3a]'>
+          <input className='w-full outline-none ' type="text" placeholder='Send a message' />
+          <button><img src={send} alt="" /></button>
+        </div>
       </div>
     </div>
   )
