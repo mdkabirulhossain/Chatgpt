34681d4634ceba85160d6dd3bb0157880b87aaf5
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-06-28 12:49:58 +0600

    Sidebar is completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,38 @@
 import { useState } from 'react'
 import './App.css'
+import logo from './assets/chatgpt.svg'
+import addimg from './assets/add-30.png'
+import msgicon from './assets/message.svg'
+import home from './assets/home.svg'
+import saved from './assets/bookmark.svg'
+import upgrade from './assets/rocket.svg'
 
 function App() {
 
   return (
-    <>
-      <h1 className='text-2xl'>HI</h1>
-    </>
+    <div className='min-h-screen flex'>
+      <div className="sidebar w-96 flex flex-col  gap-0 border-r border-r-red-50">
+        <div className="uperSide p-10 h-2/3 ">
+          <div className='flex gap-1 items-center mb-10'>
+            <img src={logo} alt="" />
+            <span className='text-3xl'>ChatGPT</span>
+          </div>
+          <button className='flex gap-1 p-3 items-center justify-center w-full mx-auto rounded-md bg-[#5A4BFF] mb-12'><img src={addimg} className='w-7 h-7' alt="" /><span className='text-xl'>New Chat</span></button>
+          <div className='flex flex-col gap-2'>
+            <button className='flex gap-4 items-center p-6 rounded-md border border-[rgba(98,98,98,1)]'><img src={msgicon} alt="" /> What is Programming ?</button>
+            <button className='flex gap-4 items-center p-6 rounded-md border border-[rgba(98,98,98,1)]'><img src={msgicon} alt="" /> How to use an API ?</button>
+          </div>
+        </div>
+        <div className="lowerSide p-10 border-t border-t-red-50">
+            <div className='flex gap-4 items-center m-3 py-1'><img src={home} className='min-w-5' alt="" />Home</div>
+            <div className='flex gap-4 items-center m-3 py-1'><img src={saved} className='min-w-5' alt="" />Saved</div>
+            <div className='flex gap-4 items-center m-3 py-1'><img src={upgrade} className='min-w-5' alt="" />Upgrade to Pro</div>
+        </div>
+      </div>
+      <div className="main">
+               <h1>hi</h1>
+      </div>
+    </div>
   )
 }
 
